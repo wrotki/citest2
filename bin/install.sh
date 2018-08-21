fc5c2d8841a18a9ea1fa5877f8e3aad2b5b763d8
Mariusz Borsa <mborsa@polyverse.io>
2018-08-20 20:11:57 -0700

    Try to install Vagrant

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+set -v
+set -x
+
 ls -al /etc
 
 cat /etc/*release
@@ -10,4 +14,8 @@ export PATH=$PATH:/usr/local/bin
 
 sudo wget -O /usr/local/bin/vagrant_2.1.2_linux_amd64.zip https://releases.hashicorp.com/vagrant/2.1.2/vagrant_2.1.2_linux_amd64.zip
 sudo unzip /usr/local/bin/vagrant_2.1.2_linux_amd64.zip
+
+ls -al /usr/local/bin
+
+echo $PATH
 vagrant list

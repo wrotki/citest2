1c5b969f88ecfae5511410d98694a70b323eda34
Mariusz Borsa <mborsa@polyverse.io>
2018-08-20 20:38:19 -0700

    Try to up the AWS Vagrant instance

--- bin/install.sh
+++ bin/install.sh
@@ -11,3 +11,24 @@ sudo unzip -d /usr/local/bin vagrant_2.1.2_linux_amd64.zip
 
 vagrant plugin install pkg-config
 vagrant plugin install vagrant-aws
+
+echo vagrant-aws plugin installation complete
+
+cat << EOF > ./Vagrantfile
+Vagrant.configure("2") do |config|
+  config.vm.box = "dummy"
+  config.vm.provider "aws" do |aws,override|
+    aws.access_key_id = "$ACCESS_KEY_ID"
+    aws.secret_access_key = "$SECRET_ACCESS_KEY"
+    aws.keypair_name = "PolyverseDevelopmentKey"
+    aws.instance_type = "t2.micro"
+    aws.region = "us-west-2"
+    aws.ami = "ami-01811acf3c02e823a"
+    aws.subnet_id = "subnet-d51285b1"
+    override.ssh.username = "ec2-user"
+    override.ssh.private_key_path = "~/.ssh/PolyverseDevelopmentKey.pem"
+  end
+end
+EOF
+
+vagrant up --provider=aws

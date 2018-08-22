72c6eb49536f955db284ea32e9eff62519fe0d04
Mariusz Borsa <mborsa@polyverse.io>
2018-08-22 14:50:34 -0700

    Install Polyverse needs couple sudos

--- bin/polyverse.sh
+++ bin/polyverse.sh
@@ -7,12 +7,12 @@ set -x
 sudo yum update -y && sudo yum upgrade -y
 
 echo "Reinstalling before Polyverse"
-yum reinstall -y \*
+sudo yum reinstall -y \*
 
 echo "Installing Polyverse"
-bash -c "curl https://repo.polyverse.io/install.sh | sh -s czcw7pjshny8lzzog8bgiizfr mborsa@polyverse.io"
+sudo bash -c "curl https://repo.polyverse.io/install.sh | sh -s czcw7pjshny8lzzog8bgiizfr mborsa@polyverse.io"
 echo "Installed Polyverse"
 
 echo "Reinstalling after Polyverse"
-yum reinstall -y \*
+sudo yum reinstall -y \*
 echo "Reinstalling after Polyverse completed"

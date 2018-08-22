10ca36dc86dd0368c386635fb70000f3da8157ea
Mariusz Borsa <mborsa@polyverse.io>
2018-08-22 14:42:21 -0700

    Install Polyverse - add some traces

--- bin/polyverse.sh
+++ bin/polyverse.sh
@@ -6,8 +6,13 @@ set -x
 
 sudo yum update -y && sudo yum upgrade -y
 
+echo "Reinstalling before Polyverse"
 yum reinstall -y \*
 
-curl https://repo.polyverse.io/install.sh | sh -s czcw7pjshny8lzzog8bgiizfr mborsa@polyverse.io
+echo "Installing Polyverse"
+bash -c "curl https://repo.polyverse.io/install.sh | sh -s czcw7pjshny8lzzog8bgiizfr mborsa@polyverse.io"
+echo "Installed Polyverse"
 
+echo "Reinstalling after Polyverse"
 yum reinstall -y \*
+echo "Reinstalling after Polyverse completed"
